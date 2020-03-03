229cfa21381242998081074c6eb94503db8f10f8
lemoneqqq <56324541+lemoneqqq@users.noreply.github.com>
2020-03-03 14:32:18 -0800

    Update capstone1.py

--- capstone1.py
+++ capstone1.py
@@ -8,14 +8,16 @@ from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 stop_words.extend(['every','onto','whose','could'])
 stop_words = set(stop_words)
+
+
 def clean_syno(text)->pd:
+    ###load the csv file and then turn them into lower cases, clean some noise like ~ xxx.more, (c) disney offical etc.
     file = csv.reader(open(text)) # Here your csv file
-    lines = list(file)
-    for  movie_info in lines[1:]:
-        #print(movie_info[-1])
+    lines = list(file) #read into list.
+    for  movie_info in lines[1:]:#disregard the first line --> column title
         syno = movie_info[-1].lower()
         syno = syno.split()
-
+        #what each line looks like:
         #['\ufeffrentrak_title_id', 'title_name', 'year', 'week', 'studio', 'genre', 'screen_number', 'sequel', 'gbo','opening', 'lead_actor', 'supporting_actor', 'producer', 'director', 'synopsis']
         #['809903', 'Daybreakers', '2010', '2', 'Other', 'Action', '2523', '0', '30101577', '15146692','Ethan Hawke,Willem Dafoe', '', 'Bryan Furst/Chris Brown/Sean Furst', 'Michael Spierig/Peter Spierig', " Fresh off the success of their inventive take on the zombie genre, Undead masterminds Michael and Peter Spierig direct Ethan Hawke in an ambitious tale of a futuristic Earth populated entirely by vampires, and the efforts made by the creatures to ensure that their food supply doesn't run out as humankind is faced with extinction. The year is 2017, and a vampire plague has turned most of the planet's human population into bloodsucking ghouls. As the population of mortals fast begins to dwindle, a vampiric corporation sets out to capture and farm every remaining human while simultaneously researching a consumable blood substitute, headed by undead hematologist Edward Dalton (Ethan Hawke). His work is interrupted after stumbling onto a pocket of human survivors lead by Elvis (Willem Dafoe), a former vampire, whose past reveals a cure that could reverse the tide and save the human race. With time running out, Dalton's only hope lies in outsmarting the security forces of his boss (Sam Neill), whose goal isn't just to find a substitute, but to repopulate humanity in order to sell its blood to the highest bidder. ~ Jason Buchanan, Rovimore "]
         without_c = look_for_C_in_syno(syno)
@@ -27,23 +29,28 @@ def clean_syno(text)->pd:
             new_syno = ''
             for letter in new:
                 new_syno += ' '+letter
-            new_syno = re.sub(r'[^\w\s]',' ',new_syno)
+            #############new_syno = re.sub(r'[^\w\s]',' ',new_syno)
             movie_info[-1] = new_syno
         #print(movie_info[-1]) """ will print out every syno, without punction and all lower"""
     return lines
+
+
 def look_for_wave_in_syno(synopisis:list)->list:
+    ### look to ~ and delete all leters after this symbol
     try:
         return synopisis[:synopisis.index('~')]
     except:
         return synopisis
 
 def look_for_C_in_syno(synopisis:list)->list:
+    """look for (c) then delete everything after it."""
     try:
         return synopisis[:synopisis.index('(c)')]
     except:
         return synopisis
 
 def reduce_more(synopisis:list)->list:
+    """in some syno, read.more is the last word, we need to delete it."""
     if 'more' in synopisis[-1]:
         return synopisis[:-1]
     else:
@@ -51,6 +58,7 @@ def reduce_more(synopisis:list)->list:
 
 
 def movie_classfier(movie_list:list)-> dict:
+    ### classfy movies by genre, store into ids.
     genre_list = defaultdict()
     movie_genre = ['Action','Adventure','Animation','Comedy','Documentary','Drama','Family',
                    'Horror','Musical','Romantic Comedy','Science Fiction','Suspense','Western',]
@@ -69,41 +77,54 @@ def get_sgle_movie(genre_combo:list)->list:### get movie info based on genre.
     return movies
 
 def nltk_token(movies:list)->list:
-    words = []
+    ### for synopisis, clean up into sentence token- then work token, return word into orgianl form(lemmatize)
+
     origianl_form = []
-    movie_syno_str = ''
-    tag = []
+    movie_syno_str = []
     result = []
     #syno_for_certain_genre = []
     for movie in movies:
-        movie_syno_str += movie[-1] + ' '
-    sentences = nltk.sent_tokenize(movie_syno_str,'english')
-    for sentence in sentences:
-        words.append(nltk.word_tokenize(sentence))
-    for element in words:
-        tag.append(nltk.pos_tag(element,lang = 'eng'))
-    #print(tag)
-    lemma = nltk.wordnet.WordNetLemmatizer()
-    for word_type in tag:
-        for word, type in word_type:
-            if type.startswith('NN'):
-                result.append(lemma.lemmatize(word, pos='n'))
-            elif type.startswith('V'):
-                result.append(lemma.lemmatize(word, pos='v'))
-            elif type.startswith('JJ'):
-                result.append(lemma.lemmatize(word, pos='a'))
-            elif type.startswith('R'):
-                result.append(lemma.lemmatize(word, pos='r'))
-            else:
-                result.append(word)
+        cleaned = []
+        movie_syno_str = movie[-1]
+        words = []
+        tag = []
+        syno_sentences = nltk.sent_tokenize(movie_syno_str,'english') ### tokenize the word.
+        # sentences.append(syno_sentences)
+        # #print(sentences)
+        for sentence in syno_sentences:
+            words.append(nltk.word_tokenize(sentence))
+            for element in words:
+                tag.append(nltk.pos_tag(element,lang = 'eng')) ## give each word a tag. so that we know which word is noun, adj etc.
+            lemma = nltk.wordnet.WordNetLemmatizer()
+            for word_type in tag:
+                for word, type in word_type:
+                    if type.startswith('NN'):#### currently we only keep n, verb, adj, and return these words into it's origianl form:
+                # likes -> like    leaves -> leaf
+                        cleaned.append(lemma.lemmatize(word, pos='n'))
+                    elif type.startswith('V'):
+                        cleaned.append(lemma.lemmatize(word, pos='v'))
+                    elif type.startswith('JJ'):
+                        cleaned.append(lemma.lemmatize(word, pos='a'))
+                    elif type.startswith('R'):
+                        cleaned.append(lemma.lemmatize(word, pos='r'))
+                    else:
+                        # it is other form, disregard it.
+                        cleaned.append(word)
+        result.append(cleaned)
     return result
 
 
 def remove_stop(text:list)->list:
     result= []
-    for element in text:
-        if element not in stop_words:
-            result.append(element)
+    for movie_syno in text:
+        clean_syno = []
+        #print(element)
+        for sentence in movie_syno:
+            sentence = re.sub(r'[^\w\s]', '', sentence)
+            if sentence != '':
+                if sentence not in stop_words:
+                    clean_syno.append(sentence)
+        result.append(clean_syno)
     return result
 
 
@@ -114,16 +135,18 @@ if __name__ == "__main__":
     total_movie_info = clean_syno('Capstone 2020 - Disney - Movie data.csv')
     #print(movie_info[0])
     genres = movie_classfier(total_movie_info)
+    #seperaete movies into different genre with their ids.
     Action_ids,Adventure_ids,Animation_ids,Comedy_ids,Documentary_ids,Drama_ids,Family_ids,\
-    Horror_ids,Musical_ids,Romantic_Comedy_ids,Science_Fiction_ids,Suspense_ids,Western_ids= genres['Action'],genres['Adventure'],\
+    Horror_ids,Musical_ids,Romantic_Comedy_ids,Science_Fiction_ids,Suspense_ids,Western_ids   =    genres['Action'],genres['Adventure'],\
                                                                                                    genres['Animation'],genres['Comedy'],\
                                                                                                    genres['Documentary'],genres['Drama'],genres['Family'],\
                                                                                                    genres['Horror'],genres['Musical'],genres['Romantic Comedy'],\
                                                                                                    genres['Science Fiction'],genres['Suspense'],genres['Western']
-    Western_movie = get_sgle_movie(Western_ids)
+    Science_Fiction_movies = get_sgle_movie(Science_Fiction_ids)
     #print(action_movie)
-    Western_movie_text = nltk_token(Western_movie)
-    Western_movie__clean_text = remove_stop(Western_movie_text)
-    print(Western_movie__clean_text)
+    Science_Fiction_text = nltk_token(Science_Fiction_movies)
+    #print(len(Science_Fiction_text))
+    Science_Fiction_text = remove_stop(Science_Fiction_text)
+    print(Science_Fiction_text)
 
 
